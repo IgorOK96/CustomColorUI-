2044a6622cc5ef1a32cf0bd01455d5fdb6b52f23
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-10-13 09:06:09 +0200

    Initial Commit

---
+++ CustomColorUI/CustomColorUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomColorUIApp.swift
+//  CustomColorUI
+//
+//  Created by user246073 on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomColorUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
